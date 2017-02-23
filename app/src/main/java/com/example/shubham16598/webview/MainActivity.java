0d242df8f726f250e7adcb9104206690f523fc6c
Shubham16598 <shubham16598@gmail.com>
2017-02-23 23:30:30 +0530

    webview app

---
+++ app/src/main/java/com/example/shubham16598/webview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.shubham16598.webview;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b = (Button)findViewById(R.id.button);
+    b.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.co.in"))  ;      }
+    sta
+    });
+    }
+}
